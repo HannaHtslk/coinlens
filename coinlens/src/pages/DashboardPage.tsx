e4482d40f9725bc6eaf755975bd5f649984f2935
annahutsaliuk <hannah.htslk@gmail.com>
2026-01-31 16:12:08 +0200

    add search and sorting to market dashboard

--- coinlens/src/pages/DashboardPage.tsx
+++ coinlens/src/pages/DashboardPage.tsx
@@ -1,14 +1,70 @@
 import { useGetTopCoinsQuery } from "../api/cryptoApi";
+import { useState, useMemo } from "react";
 
 const DashboardPage = () => {
     const { data, isLoading, error } = useGetTopCoinsQuery();
+    const [search, setSearch] = useState("");
+    const [sortKey, setSortKey] = useState<
+        "price" | "marketCap" | "change24h" | null
+    >(null);
 
     if (isLoading) return <p>Loading...</p>;
     if (error) return <p>Error loading data</p>;
 
+    const filteredAndSortedData = useMemo(() => {
+        if (!data) return [];
+
+        let result = data.filter((coin) =>
+            coin.name.toLowerCase().includes(search.toLowerCase())
+        );
+
+        if (sortKey === "price") {
+            result = [...result].sort(
+                (a, b) => b.current_price - a.current_price
+            );
+        }
+
+        if (sortKey === "marketCap") {
+            result = [...result].sort(
+                (a, b) => b.market_cap - a.market_cap
+            );
+        }
+
+        if (sortKey === "change24h") {
+            result = [...result].sort(
+                (a, b) =>
+                    (b.price_change_percentage_24h ?? 0) -
+                    (a.price_change_percentage_24h ?? 0)
+            );
+        }
+
+        return result;
+    }, [data, search, sortKey]);
+
+
     return (
         <div>
             <h1>Market</h1>
+
+            <div style={{ marginBottom: "16px" }}>
+                <input
+                    type="text"
+                    placeholder="Search coin..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+
+                <div style={{ marginTop: "8px" }}>
+                    <button onClick={() => setSortKey("price")}>Sort by Price</button>
+                    <button onClick={() => setSortKey("marketCap")}>
+                        Sort by Market Cap
+                    </button>
+                    <button onClick={() => setSortKey("change24h")}>
+                        Sort by 24h %
+                    </button>
+                </div>
+            </div>
+
 
             <table>
                 <thead>
@@ -21,7 +77,7 @@ const DashboardPage = () => {
                 </thead>
 
                 <tbody>
-                    {data?.map((coin) => (
+                    {filteredAndSortedData?.map((coin) => (
                         <tr key={coin.id}>
                             <td>{coin.name}</td>
                             <td align="right">
