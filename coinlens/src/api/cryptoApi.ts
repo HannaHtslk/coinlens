2b9c41b19944a6f7999184cca158edc367857094
annahutsaliuk <hannah.htslk@gmail.com>
2026-01-31 17:41:29 +0200

    fixed ts errors

--- coinlens/src/api/cryptoApi.ts
+++ coinlens/src/api/cryptoApi.ts
@@ -1,4 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  type CoinMarket,
+  type CoinDetails,
+  type MarketChartResponse,
+} from "../types/coin";
 
 export const cryptoApi = createApi({
   reducerPath: "cryptoApi",
@@ -6,18 +11,25 @@ export const cryptoApi = createApi({
     baseUrl: "https://api.coingecko.com/api/v3/",
   }),
   endpoints: (builder) => ({
-    getTopCoins: builder.query<any[], void>({
+    getTopCoins: builder.query<CoinMarket[], void>({
       query: () =>
         "coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1",
     }),
-    getCoinById: builder.query<any, string>({
+    getCoinById: builder.query<CoinDetails, string>({
       query: (coinId) => `coins/${coinId}`,
     }),
-    getCoinMarketChart: builder.query<any, { coinId: string, days: number }>({
+    getCoinMarketChart: builder.query<
+      MarketChartResponse,
+      { coinId: string; days: number }
+    >({
       query: ({ coinId, days }) =>
         `coins/${coinId}/market_chart?vs_currency=usd&days=${days}`,
     }),
   }),
 });
 
-export const { useGetTopCoinsQuery, useGetCoinByIdQuery, useGetCoinMarketChartQuery, } = cryptoApi;
+export const {
+  useGetTopCoinsQuery,
+  useGetCoinByIdQuery,
+  useGetCoinMarketChartQuery,
+} = cryptoApi;
---
+++ coinlens/src/types/coin.ts
@@ -0,0 +1,26 @@
+export type CoinMarket = {
+  id: string;
+  name: string;
+  symbol: string;
+  current_price: number;
+  market_cap: number;
+  price_change_percentage_24h: number | null;
+};
+
+export type CoinDetails = {
+  id: string;
+  name: string;
+  symbol: string;
+  market_data: {
+    current_price: {
+      usd: number;
+    };
+    market_cap: {
+      usd: number;
+    };
+  };
+};
+
+export type MarketChartResponse = {
+  prices: [number, number][];
+};
