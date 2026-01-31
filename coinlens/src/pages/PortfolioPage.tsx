c05650e14bf6c3e47f441f1653badb8834162f36
annahutsaliuk <hannah.htslk@gmail.com>
2026-01-31 17:18:50 +0200

    verify portfolio actions and persistence

--- coinlens/src/pages/PortfolioPage.tsx
+++ coinlens/src/pages/PortfolioPage.tsx
@@ -1,5 +1,28 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { addToPortfolio } from "../redux/portfolio/portfolioSlice";
+
 const PortfolioPage = () => {
-    return <h1>Portfolio</h1>;
+  const dispatch = useAppDispatch();
+  const items = useAppSelector((state) => state.portfolio.items);
+
+  useEffect(() => {
+    dispatch(
+      addToPortfolio({
+        coinId: "bitcoin",
+        name: "Bitcoin",
+        amount: 0.5,
+      })
+    );
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Portfolio</h1>
+
+      <pre>{JSON.stringify(items, null, 2)}</pre>
+    </div>
+  );
 };
 
 export default PortfolioPage;
