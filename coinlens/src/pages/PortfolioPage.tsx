df9a9fc4d4d18e7fe9b2c98bc292d9a07244f744
annahutsaliuk <hannah.htslk@gmail.com>
2026-01-31 17:21:15 +0200

    add basic portfolio table with remove action

--- coinlens/src/pages/PortfolioPage.tsx
+++ coinlens/src/pages/PortfolioPage.tsx
@@ -1,26 +1,43 @@
-import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
-import { addToPortfolio } from "../redux/portfolio/portfolioSlice";
+import { removeFromPortfolio } from "../redux/portfolio/portfolioSlice";
 
 const PortfolioPage = () => {
   const dispatch = useAppDispatch();
   const items = useAppSelector((state) => state.portfolio.items);
 
-  useEffect(() => {
-    dispatch(
-      addToPortfolio({
-        coinId: "bitcoin",
-        name: "Bitcoin",
-        amount: 0.5,
-      })
-    );
-  }, [dispatch]);
+  if (items.length === 0) {
+    return <h1>Portfolio is empty</h1>;
+  }
 
   return (
     <div>
       <h1>Portfolio</h1>
 
-      <pre>{JSON.stringify(items, null, 2)}</pre>
+      <table>
+        <thead>
+          <tr>
+            <th align="left">Coin</th>
+            <th align="right">Amount</th>
+            <th />
+          </tr>
+        </thead>
+
+        <tbody>
+          {items.map((item) => (
+            <tr key={item.coinId}>
+              <td>{item.name}</td>
+              <td align="right">{item.amount}</td>
+              <td>
+                <button
+                  onClick={() => dispatch(removeFromPortfolio(item.coinId))}
+                >
+                  Remove
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
